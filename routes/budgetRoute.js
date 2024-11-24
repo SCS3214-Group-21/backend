38173c515957ce141b13fdc383e7772a82fec6a8
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2024-11-24 16:36:01 +0530

    Create updateBudget controller

--- routes/budgetRoute.js
+++ routes/budgetRoute.js
@@ -3,6 +3,7 @@ import getMinPackages from "../controllers/Budget/getMinPackages.js";
 import createBudget from "../controllers/Budget/createBudget.js";
 import getBudget from "../controllers/Budget/getBudget.js";
 import getInitialBudget from "../controllers/Budget/getInitialBudget.js";
+import updateBudget from "../controllers/Budget/updateBudget.js";
 import { isAuthorize } from "../middlewares/auth.js";
 
 const vendorRouter = express.Router();
@@ -11,5 +12,6 @@ vendorRouter.get("/get-min", isAuthorize, getMinPackages);
 vendorRouter.post("/create-budget", isAuthorize, createBudget);
 vendorRouter.get("/get-budget/:id", isAuthorize, getBudget);
 vendorRouter.get("/get-initial/:id", isAuthorize, getInitialBudget);
+vendorRouter.put("/update-budget/:plan_id", isAuthorize, updateBudget);
 
 export default vendorRouter;
