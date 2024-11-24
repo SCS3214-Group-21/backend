92af5c6af1e1b7865dec6e69d1bf64ac0af5b205
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2024-11-24 16:24:25 +0530

    Create getInitialBudget controller

--- routes/budgetRoute.js
+++ routes/budgetRoute.js
@@ -2,12 +2,14 @@ import express from "express";
 import getMinPackages from "../controllers/Budget/getMinPackages.js";
 import createBudget from "../controllers/Budget/createBudget.js";
 import getBudget from "../controllers/Budget/getBudget.js";
+import getInitialBudget from "../controllers/Budget/getInitialBudget.js";
 import { isAuthorize } from "../middlewares/auth.js";
 
 const vendorRouter = express.Router();
 
 vendorRouter.get("/get-min", isAuthorize, getMinPackages);
 vendorRouter.post("/create-budget", isAuthorize, createBudget);
-vendorRouter.get("/get-budget/:id", isAuthorize, getBudget)
+vendorRouter.get("/get-budget/:id", isAuthorize, getBudget);
+vendorRouter.get("/get-initial/:id", isAuthorize, getInitialBudget);
 
 export default vendorRouter;
