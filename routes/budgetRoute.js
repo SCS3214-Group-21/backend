7b723e4b3f4c610dcf42e766c4785ca44e2cdaeb
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2024-11-24 12:58:46 +0530

    Create createBudget Controller

--- routes/budgetRoute.js
+++ routes/budgetRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
 import getMinPackages from "../controllers/Budget/getMinPackages.js";
+import createBudget from "../controllers/Budget/createBudget.js";
 import { isAuthorize } from "../middlewares/auth.js";
 
 const vendorRouter = express.Router();
 
 vendorRouter.get("/get-min", isAuthorize, getMinPackages);
+vendorRouter.post("/create-budget", isAuthorize, createBudget);
 
 export default vendorRouter;
