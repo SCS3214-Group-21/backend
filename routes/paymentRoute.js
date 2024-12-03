535e2f1582d220095e85d60889b10d10606b7f72
Binuri Mindula <binuriwagarachchi@gmail.com>
2024-12-04 00:47:51 +0530

    Resolve merge issues and add paid status for booking

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createCheckoutSession } from '../controllers/payment/doPayment.js';
+import { isAuthorize } from '../middlewares/auth.js';
 
 const router = express.Router();
 
 // Create checkout session route
-router.post('/create-checkout-session', createCheckoutSession);
+router.post('/create-checkout-session',isAuthorize, createCheckoutSession);
 
 export default router;
 
